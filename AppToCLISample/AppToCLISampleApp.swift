df1d4582a847da9a893087c76e25c0da7e10f1da
Trevin Wisaksana <twisaksana@gmail.com>
2023-03-24 07:30:04 +0700

    Initial Commit

---
+++ AppToCLISample/AppToCLISampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppToCLISampleApp.swift
+//  AppToCLISample
+//
+//  Created by Trevin Wisaksana on 24/03/23.
+//
+
+import SwiftUI
+
+@main
+struct AppToCLISampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
